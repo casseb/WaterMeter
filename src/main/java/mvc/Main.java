9cdd810b5bbc78797eac03a64f580782c139a0c2
casseb <felipe.casseb@gmail.com>
2017-07-23 11:51:56 -0300

    Testando criação de pdf

--- src/main/java/mvc/Main.java
+++ src/main/java/mvc/Main.java
@@ -2,8 +2,12 @@ package mvc;
 
 import static spark.Spark.*;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -11,6 +15,11 @@ import java.util.concurrent.TimeUnit;
 
 import org.eclipse.jetty.server.session.JDBCSessionManager.Session;
 
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.TelegramBotAdapter;
 import com.pengrad.telegrambot.request.SendMessage;
@@ -25,6 +34,8 @@ import objects.basic.PersonType;
 import objects.basic.Route;
 import objects.basic.RouteGroup;
 import objects.basic.ScheduleMessage;
+import objects.files.BoxFileObject;
+import objects.files.BoxFolderObject;
 
 public class Main {
 	
@@ -59,6 +70,7 @@ public class Main {
 		
 		permissaoADM();
 		triggerMessages();
+		testCreatePDF();
 		
     }
 
@@ -92,7 +104,53 @@ public class Main {
 		
 	}
 	
-	
+	private static void testCreatePDF() {
+		try {
+			Document document = new Document();
+			File file = new File("src/main/java/objects/basic/tempFiles/PDF_DevMedia.pdf");
+			OutputStream fileOut = new FileOutputStream(file);
+
+			Font font20 = new Font();
+			font20.setSize(20);
+			Font fontbold = new Font();
+			fontbold.setStyle(fontbold.BOLD);
+
+			Paragraph paragraph1 = new Paragraph();
+			paragraph1.add(new Chunk("Teste1", font20));
+			Paragraph paragraph2 = new Paragraph();
+			paragraph2.add(new Chunk("Teste2", fontbold));
+			Paragraph paragraph3 = new Paragraph();
+			paragraph3.add("Teste3\n");
+			
+			PdfWriter.getInstance(document, fileOut);
+			document.open();
+			document.add(paragraph1);
+			document.add(paragraph2);
+			document.add(paragraph3);
+			document.close();
+			
+			List<String> folder = new LinkedList();
+			folder.add("Pasta1");
+			folder.add("Pasta2");
+			BoxFolderObject currentFolder = null;
+			BoxFolderObject rootFolder = null;
+
+			for (String string : folder) {
+				currentFolder = model.locateBoxFolderObjectByName(string);
+				if (currentFolder == null) {
+					currentFolder = new BoxFolderObject(string, model, rootFolder);
+				}
+				rootFolder = currentFolder;
+			}
+			
+			model.addBoxFileObject(new BoxFileObject("Teste1",model,file,currentFolder));
+			
+
+		} catch (Exception de) {
+			System.err.println(de.getMessage());
+		} 
+
+	}
 	
 	
 	
