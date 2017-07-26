addbd5c526ec872d03b692a0ba75a4097f617bbd
casseb <felipe.casseb@gmail.com>
2017-07-26 19:51:07 -0300

    Testando aceite de termos

--- src/main/java/dialogs/termos/DialogReadTermos.java
+++ src/main/java/dialogs/termos/DialogReadTermos.java
@@ -30,7 +30,7 @@ public class DialogReadTermos extends Dialog {
 
 		if (nextStep()) {
 			answer.append("Selecione o tópico do termo que deseja ler");
-			sendFile(model.box.getFileByBox(model.locateBoxFileObjectByName("Termos.pdf", "Termos")));
+			sendFile(model.box.getFileByBox(model.locateBoxFileObjectByName("Termos - " + model.administracao.versaoTermo(), "Termos", "Versões")));
 			prepareKeyboard(keyboard, 2);
 
 			return finishStep();
